7b65a13b79e9d16ed05b3ab66fa9045f8d67c1db
lan <quw112233456@gmail.com>
2023-05-13 12:21:35 +0800

    adding dist

---
+++ frontend/dist/assets/ContentLayout-766c51ae.js
@@ -0,0 +1,1 @@
+import{_ as r,o,e as s,b as t,i as a}from"./index-98a08bd1.js";const n={},c={class:"w-full p-4 lg:pl-72 dark:bg-gray-900 select-none"},d={class:"p-4 border-2 border-gray-200 rounded-lg dark:border-gray-700"};function l(e,_){return o(),s("div",c,[t("div",d,[a(e.$slots,"default")])])}const f=r(n,[["render",l]]);export{f as C};
