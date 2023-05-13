f43671afd41ab3c8835a4f64a50d2d20429497a0
lan <quw112233456@gmail.com>
2023-05-13 16:12:32 +0800

    update nginx config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/deploying-vite-project-example/',
     plugins: [vue()],
     resolve: {
         alias: {
